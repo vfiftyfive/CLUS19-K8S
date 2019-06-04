2d2772a6d608f0b1b9d17e52391339dea7399286
vfiftyfive <vfiftyfive@cisco.com>
2019-06-04 10:40:25 +0100

    change test script

--- Helper/kubeSendApp.sh
+++ Helper/kubeSendApp.sh
@@ -1,2 +1,5 @@
+#!/bin/sh
+echo $(pwd)
 pod=$(KUBECONFIG=$(pwd)/config kubectl get pod | grep guest | cut -d " " -f 1 | head -n 1)
+echo ${pod}
 KUBECONFIG=$(pwd)/config kubectl exec -it ${pod} -- /home/integrationTest.sh
