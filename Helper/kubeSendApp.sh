bf70d69fa452480cabbab90790e25b41173ce7d2
vfiftyfive <vfiftyfive@cisco.com>
2019-06-04 10:08:20 +0100

    change validation script

--- Helper/kubeSendApp.sh
+++ Helper/kubeSendApp.sh
@@ -1,1 +1,2 @@
-kubectl exec -it $(kubectl get pod | grep guest | cut -d " " -f 1 | head -n 1) -- /home/integrationTest.sh
+pod=$(KUBECONFIG=$(pwd)/config kubectl get pod | grep guest | cut -d " " -f 1 | head -n 1)
+KUBECONFIG=$(pwd)/config kubectl exec -it ${pod} -- /home/integrationTest.sh
